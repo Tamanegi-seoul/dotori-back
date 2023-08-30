7cbce95f94026bf3a2bad0ae06b3f3c5ff70d24c
Joonwoo Park <93jpark@gmail.com>
2023-08-30 20:32:04 +0900

    initialize project

---
+++ src/main/java/protokoll/eatscape/EatscapeApplication.java
@@ -0,0 +1,13 @@
+package protokoll.eatscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatscapeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatscapeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/protokoll/eatscape/EatscapeApplicationTests.java
@@ -0,0 +1,13 @@
+package protokoll.eatscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatscapeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
